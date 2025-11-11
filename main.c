ae61705e8aa9fadf3dc2522206334919344c67bd
Francis Devassy <1876@trenser.com>
2025-11-11 15:23:17 +0530

    main.c to print hello world and .gitignore for executables

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
-//**************************** PROJECT PRINT HELLO WORLD ******************************
-// Copyright (c) 2021 Trenser
+//**************************** PROJECT GitFamiliarization *********************
+// Copyright (c) 2025 Trenser
 // All Rights Reserved
 //*****************************************************************************
 //
-// File : main.c
+// File    : main.c
 // Summary : Program to print Hello World
-// Note : Program to print Hello World
-// Author :Francis V D
-// Date :11-11-2025
+// Note    : Program to print Hello World
+// Author  :Francis V D
+// Date    :11-11-2025
 //
 //*****************************************************************************
-
+//******************************* Include Files *******************************
 #include <stdio.h>
+//******************************* Local Types *********************************
+//***************************** Local Constants *******************************
 #define RETURN_ZERO 0
-
+//***************************** Local Variables *******************************
+//****************************** Local Functions ******************************
 int main(void) 
 {
     printf("Hello, World!\n");
     return RETURN_ZERO;
 }
-
-//EOF
+//*********************** Class Method Implementations ************************
+// EOF
